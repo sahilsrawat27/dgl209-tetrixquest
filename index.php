bf1b5027fe7335dd4ab320552d597be37658f991
SahilSRawat <simransahil2113@gmail.com>
2024-02-10 12:20:49 -0800

    Create game homepage

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tetrix Quest - Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="game_title">Tetrix Quest</h1>
+        <p class="game_quote">Choose your adventure.</p>
+        <div class="menu-container">
+            <button class="tetris-btn">Start Game</button>
+            <button class="tetris-btn">Tournaments</button>
+            <button class="tetris-btn">High Scores</button>
+            <button class="tetris-btn">Instructions</button>
+            <button class="tetris-btn">Exit Game</button>
+        </div>
+    </div>
+
+</body>
+
+</html>
