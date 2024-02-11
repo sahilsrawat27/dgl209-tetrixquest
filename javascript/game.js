6f5d76acc0f6a58b2b32274b00a566a3bbabe220
SahilSRawat <simransahil2113@gmail.com>
2024-02-11 11:38:28 -0800

    Add the blocks

--- javascript/game.js
+++ javascript/game.js
@@ -57,6 +57,52 @@ document.addEventListener("DOMContentLoaded", () => {
     drawGrid(); // Draw the background grid.
     drawMatrix(arena, { x: 0, y: 0 }); // Draw the static blocks on the arena.
   }
+  //Create the blocks
+  function createPiece(type) {
+    if (type === "T") {
+      return [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+      ];
+    } else if (type === "O") {
+      return [
+        [2, 2],
+        [2, 2],
+      ];
+    } else if (type === "L") {
+      return [
+        [0, 3, 0],
+        [0, 3, 0],
+        [0, 3, 3],
+      ];
+    } else if (type === "J") {
+      return [
+        [0, 4, 0],
+        [0, 4, 0],
+        [4, 4, 0],
+      ];
+    } else if (type === "I") {
+      return [
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+        [0, 5, 0, 0],
+      ];
+    } else if (type === "S") {
+      return [
+        [0, 6, 6],
+        [6, 6, 0],
+        [0, 0, 0],
+      ];
+    } else if (type === "Z") {
+      return [
+        [7, 7, 0],
+        [0, 7, 7],
+        [0, 0, 0],
+      ];
+    }
+  }
 
   // Initialize the arena matrix with a call to createMatrix.
   const arena = createMatrix(12, 20);
