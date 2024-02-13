f1e787bb53011685a3be75f0756bcbcf4f7dd178
SahilSRawat <simransahil2113@gmail.com>
2024-02-12 18:39:53 -0800

    Create a progress page

---
+++ tournaments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tetrix Quest</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="game_title">Tetrix Quest</h1>
+        <p class="game_quote">Embark on a puzzle adventure.</p>
+        <div>
+            In Progress...............
+        </div>
+    </div>
+</body>
+
+</html>
